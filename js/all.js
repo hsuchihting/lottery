edb8bda13cecbef9eb05380ab3545285ffc50903
TimHsu <hsuchihting@gmail.com>
2020-06-05 21:58:26 +0800

    lottery

--- js/all.js
+++ js/all.js
@@ -1,15 +1,15 @@
 $(document).ready(function () {
 	destiny_txt = new Array(6);
-	destiny_txt[0] = "大吉";
-	destiny_txt[1] = "中吉";
-	destiny_txt[2] = "小吉";
-	destiny_txt[3] = "凶";
-	destiny_txt[4] = "平";
-	destiny_txt[5] = "大凶";
+	destiny_txt[0] = "Charlie, Mercedes";
+	destiny_txt[1] = "Brandon, Linda";
+	destiny_txt[2] = "俊哲，岱娜";
+	destiny_txt[3] = "智庭，旻旻";
+	destiny_txt[4] = "Marc，亮亮";
+	destiny_txt[5] = "東岳，淑玲";
 
 	index = Math.floor(Math.random() * destiny_txt.length); //js亂數程式
 
-	$(".btn").on("click", function (e) {
+	$(".jq-result").on("click", function (e) {
 		$(".result").append(destiny_txt[index]);
 		$(this).remove();
 		$(".restart").show();
